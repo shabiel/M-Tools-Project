3b3389b49e766323877eb784ba493d78688e2ac6
Sam Habiel <sam.habiel@gmail.com>
2017-03-29 05:16:24 -0700

    Date required but doesn't have to conform to any format

--- us.pwc.vista.eclipse.wizard/src/us/pwc/vista/eclipse/wizard/core/NewFileTopLinesPage.java
+++ us.pwc.vista.eclipse.wizard/src/us/pwc/vista/eclipse/wizard/core/NewFileTopLinesPage.java
@@ -1,3 +1,21 @@
+//---------------------------------------------------------------------------
+// Copyright 2013 PwC
+// Copyright 2017 Sam Habiel (sam dot habiel at gmail dot com)
+
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//---------------------------------------------------------------------------
+
 package us.pwc.vista.eclipse.wizard.core;
 
 import org.eclipse.jface.dialogs.IInputValidator;
@@ -70,7 +88,7 @@ public class NewFileTopLinesPage extends WizardPage {
 		this.site = this.createPair(this.container, "Site:", new RequiredInputValidator());
 		this.developer = this.createPair(this.container, "Developer:", new RequiredInputValidator());
 		this.beriefDescription = this.createPair(this.container, "Brief description:", new RequiredInputValidator());
-		this.date = this.createPair(this.container, "Date:", new DateInputValidator(true));
+		this.date = this.createPair(this.container, "Date:", new RequiredInputValidator());
 		
 		this.majorVersionNumber = this.createPair(this.container, "Major version number:", new NumberInputValidator(true));
 		this.minorVersionNumber = this.createPair(this.container, "Minor version number:", new NumberInputValidator(true));
